a7305042c96a95517d2011c298e139714228a07c
João Gilberto <gjoao11.dev@gmail.com>
2021-10-28 12:11:03 -0300

    Create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Prismic from '@prismicio/client';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +31,82 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [hasNextPage, setHasNextPage] = useState(!!postsPagination.next_page);
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  function loadMorePosts(): void {
+    fetch(postsPagination.next_page)
+      .then(response => response.json())
+      .then(responseJson => {
+        if (!responseJson.next_page) {
+          setHasNextPage(false);
+        }
 
-//   // TODO
-// };
+        setPosts([...posts, ...responseJson.results]);
+      });
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+
+      <main className={`${styles.container} ${commonStyles.container}`}>
+        <img src="/images/logo.svg" alt="logo" />
+
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link key={post.uid} href={`post/${post.uid}`}>
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div className={styles.info}>
+                  <time>
+                    <FiCalendar size={20} color="#BBBBBB" />
+                    {format(new Date(post.first_publication_date), 'd MMM y', {
+                      locale: ptBR,
+                    })}
+                  </time>
+
+                  <span>
+                    <FiUser size={20} color="#BBBBBB" />
+                    {post.data.author}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+
+        {hasNextPage && (
+          <button
+            className={styles.loadMorePosts}
+            type="button"
+            onClick={loadMorePosts}
+          >
+            Carregar mais posts
+          </button>
+        )}
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    Prismic.predicates.at('document.type', 'posts'),
+    { pageSize: 20, page: 1 }
+  );
+
+  return {
+    props: {
+      postsPagination: {
+        results: postsResponse.results,
+        next_page: postsResponse.next_page,
+      },
+    },
+  };
+};
